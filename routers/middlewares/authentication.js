8f3f46842e54149a0efcdf11326cd11748792d44
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-30 18:17:46 +0300

    create authorization middleware

--- routers/middlewares/authentication.js
+++ routers/middlewares/authentication.js
@@ -8,16 +8,17 @@ const Secret = process.env.SECRET;
 
 const authentication = (req , res , next) =>{
     try{
-        if (!req.header.authentication){
-            return res.status(403).json({message: "Forbidden!"})
+        if (!req.headers.authorization) {
+          return res.status(403).json({ message: "Forbidden!" });
         }
         const token = req.headers.authorization.split(" ")[1];
         const parsedToken = jwt.verify(token, Secret);
 
+        req.token = parsedToken;
         next();
 
     }catch(err){
-        res.status(403).json(err)
+        res.status(403).json({err})
 
     }
 }
